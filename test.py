034841babfedbf1138d5764f4a871410d230086f
wonjh <clomia.sig@gmail.com>
2022-08-16 15:59:28 +0900

    upload

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import slack
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-slack_token = "xoxb-3019904477665-3737651200213-vdnNuYZ4vlGWl8qui81tZYMN"  # 발급받은 Token 값
+slack_token = "복사한 토큰을 이곳에 붙여넣기"  # 발급받은 Token 값
 client = slack.WebClient(token=slack_token)
 client.chat_postMessage(channel="#alert", text="안녕하세요 저는 알럴트라고 해요!")
